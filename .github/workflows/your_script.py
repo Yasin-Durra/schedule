32f1931c58da0f0e74da706cb950ef517702d477
Yasin-Durra <71937188+Yasin-Durra@users.noreply.github.com>
2023-05-26 01:27:31 +0300

    Update your_script.py

--- .github/workflows/your_script.py
+++ .github/workflows/your_script.py
@@ -1,1 +1,15 @@
+import datetime
 
+# Get the current date
+current_date = datetime.date.today()
+
+# Convert the date to a string representation
+date_str = current_date.strftime("%Y-%m-%d")
+
+# Open the file in append mode (creates the file if it doesn't exist)
+file_path = "date_file.txt"  # Specify the file path or name here
+with open(file_path, "a") as file:
+    # Write the date to the file
+    file.write(date_str + "\n")
+
+print("Date written to file:", date_str)
