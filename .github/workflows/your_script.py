55763d19340d4e08395cd504a539252d6c796179
Yasin-Durra <71937188+Yasin-Durra@users.noreply.github.com>
2023-05-26 11:19:46 +0300

    Update your_script.py

--- .github/workflows/your_script.py
+++ .github/workflows/your_script.py
@@ -7,7 +7,7 @@ current_date = datetime.date.today()
 date_str = current_date.strftime("%Y-%m-%d")
 
 # Open the file in append mode (creates the file if it doesn't exist)
-file_path = "date_file.txt"  # Specify the file path or name here
+file_path = "${{ github.workspace }}/.github/workflows/date_file.txt"  # Specify the file path or name here
 with open(file_path, "a") as file:
     # Write the date to the file
     file.write(date_str + "\n")
